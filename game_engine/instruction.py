9f5ee5fe46b81e6956fb276f9e463c7c87644566
Adrian-Garcia <adriangarlop@indexzero.io>
2021-04-25 21:20:02 -0500

    Added instructions class. Game now works with instructions

---
+++ game_engine/instruction.py
@@ -0,0 +1,4 @@
+class Instruction(object):
+    def __init__(self, character_name, movement):
+        self.character_name = character_name
+        self.movement = movement
--- game_engine/start.py
+++ game_engine/start.py
@@ -1,18 +1,40 @@
 import pygame
 from character import *
+from instruction import *
 
 pygame.init()
 
 clock = pygame.time.Clock()
-speed = 30
+speed = 3
 
 display_width = 500
-display_height = 300
+display_height = 500
 
-characters = [
+obj_characters = [
     #         x  y   width height speed
-    Character(0, 0,  30,   30,    10),
-    Character(0, 50, 30,   30,    10)
+    Character(0, 0,  30,   30,    50),
+    Character(0, 50, 30,   30,    50)
+]
+
+name_characters = {
+    'Rosita Fresita': obj_characters[0],
+    'Dino Adrian': obj_characters[1]
+}
+
+instructions = [
+    #           name              movement
+    Instruction('Rosita Fresita', 'right'),
+    Instruction('Rosita Fresita', 'right'),
+    Instruction('Rosita Fresita', 'right'),
+    Instruction('Rosita Fresita', 'right'),
+    Instruction('Rosita Fresita', 'right'),
+    Instruction('Rosita Fresita', 'down'),
+    Instruction('Dino Adrian',    'down'),
+    Instruction('Dino Adrian',    'down'),
+    Instruction('Dino Adrian',    'down'),
+    Instruction('Dino Adrian',    'down'),
+    Instruction('Dino Adrian',    'down'),
+    Instruction('Dino Adrian',    'right')
 ]
 
 def userMoves(character, pressed_key):
@@ -28,6 +50,22 @@ def userMoves(character, pressed_key):
     if pressed_key[pygame.K_LEFT] or pressed_key[pygame.K_a]:
         character.move_left()
 
+def instructionMoves(instruction):
+    character = name_characters[instruction.character_name]
+    movement = instruction.movement
+
+    if movement == 'down':
+        character.move_down(display_height)
+
+    if movement == 'up':
+        character.move_up()
+
+    if movement == 'right':
+        character.move_right(display_width)
+
+    if movement == 'left':
+        character.move_left()
+
 def checkIfUserQuit(pressed_key):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,20 +75,20 @@ def checkIfUserQuit(pressed_key):
         pygame.quit()
 
 display = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("Super compi que no tiene nombre todavia")
+pygame.display.set_caption('Super compi que no tiene nombre todavia')
 
 while True:
     clock.tick(speed)
     pressed_key = pygame.key.get_pressed()
-    
+
     checkIfUserQuit(pressed_key)
 
-    for character in characters:
-        userMoves(character, pressed_key)
+    if len(instructions):
+        instructionMoves(instructions.pop(0))
 
     display.fill((0, 0, 0))
 
-    for character in characters:
+    for character in obj_characters:
         pygame.draw.rect(display, (255, 255, 255), (character.x, character.y, character.width, character.height))
 
     pygame.display.update()
