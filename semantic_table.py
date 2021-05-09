b1e5de68e49ed089f9675813f3b76041d1d6ac78
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-09 14:15:34 -0500

    Used blacks on the whole repository

--- semantic_table.py
+++ semantic_table.py
@@ -1,135 +1,148 @@
 from symbol_tables import *
 
+
 class SemanticTable:
-    types = { 'INT', 'FLT', 'CHAR', 'STR', 'BOOL', 'NULL' }
+    types = {"INT", "FLT", "CHAR", "STR", "BOOL", "NULL"}
 
     #                  <     >     <=     >=
-    comparison_op = { 'LT', 'GT', 'LTE', 'GTE' }
+    comparison_op = {"LT", "GT", "LTE", "GTE"}
 
     #                    +      -      +=       -=       *=     /=     %      *=       /=       %=
-    __operations_op = { 'ADD', 'SUB', 'ADDEQ', 'SUBEQ', 'MUL', 'DIV', 'MOD', 'MULEQ', 'DIVEQ', 'MODEQ' }
+    __operations_op = {
+        "ADD",
+        "SUB",
+        "ADDEQ",
+        "SUBEQ",
+        "MUL",
+        "DIV",
+        "MOD",
+        "MULEQ",
+        "DIVEQ",
+        "MODEQ",
+    }
 
     #                ==     !=      ||    &&
-    matching_op = { 'BEQ', 'BNEQ', 'OR', 'AND' }
+    matching_op = {"BEQ", "BNEQ", "OR", "AND"}
 
     __operations = {
-        'INT' : {
-            'INT': 'INT',
-            'FLT': 'FLT',
-            'CHAR': 'INT',
-            'STR': 'error',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "INT": {
+            "INT": "INT",
+            "FLT": "FLT",
+            "CHAR": "INT",
+            "STR": "error",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'FLT': {
-            'INT': 'FLT',
-            'FLT': 'FLT',
-            'CHAR': 'FLT',
-            'STR': 'error',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "FLT": {
+            "INT": "FLT",
+            "FLT": "FLT",
+            "CHAR": "FLT",
+            "STR": "error",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'CHAR': {
-            'INT': 'INT',
-            'FLT': 'FLT',
-            'CHAR': 'STR',
-            'STR': 'STR',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "CHAR": {
+            "INT": "INT",
+            "FLT": "FLT",
+            "CHAR": "STR",
+            "STR": "STR",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'STR': {
-            'INT': 'error',
-            'FLT': 'error',
-            'CHAR': 'STR',
-            'STR': 'STR',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "STR": {
+            "INT": "error",
+            "FLT": "error",
+            "CHAR": "STR",
+            "STR": "STR",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'BOOL': {
-            'INT': 'BOOL',
-            'FLT': 'BOOL',
-            'CHAR': 'error',
-            'STR': 'error',
-            'BOOL': 'BOOL',
-            'NULL': 'error'
+        "BOOL": {
+            "INT": "BOOL",
+            "FLT": "BOOL",
+            "CHAR": "error",
+            "STR": "error",
+            "BOOL": "BOOL",
+            "NULL": "error",
         },
-
-        'NULL': {
-            'INT': 'error',
-            'FLT': 'error',
-            'CHAR': 'error',
-            'STR': 'error',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "NULL": {
+            "INT": "error",
+            "FLT": "error",
+            "CHAR": "error",
+            "STR": "error",
+            "BOOL": "error",
+            "NULL": "error",
         },
     }
 
     __comparison = {
-        'INT': {
-            'INT': 'BOOL',
-            'FLT': 'BOOL',
-            'CHAR': 'BOOL',
-            'STR': 'error',
-            'BOOL': 'BOOL',
-            'NULL': 'error'
+        "INT": {
+            "INT": "BOOL",
+            "FLT": "BOOL",
+            "CHAR": "BOOL",
+            "STR": "error",
+            "BOOL": "BOOL",
+            "NULL": "error",
         },
-
-        'FLT': {
-            'INT': 'BOOL',
-            'FLT': 'BOOL',
-            'CHAR': 'BOOL',
-            'STR': 'error',
-            'BOOL': 'BOOL',
-            'NULL': 'error'
+        "FLT": {
+            "INT": "BOOL",
+            "FLT": "BOOL",
+            "CHAR": "BOOL",
+            "STR": "error",
+            "BOOL": "BOOL",
+            "NULL": "error",
         },
-
-        'CHAR': {
-            'INT': 'BOOL',
-            'FLT': 'BOOL',
-            'CHAR': 'BOOL',
-            'STR': 'BOOL',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "CHAR": {
+            "INT": "BOOL",
+            "FLT": "BOOL",
+            "CHAR": "BOOL",
+            "STR": "BOOL",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'STR': {
-            'INT': 'error',
-            'FLT': 'error',
-            'CHAR': 'BOOL',
-            'STR': 'BOOL',
-            'BOOL': 'error',
-            'NULL': 'error'
+        "STR": {
+            "INT": "error",
+            "FLT": "error",
+            "CHAR": "BOOL",
+            "STR": "BOOL",
+            "BOOL": "error",
+            "NULL": "error",
         },
-
-        'BOOL': {
-            'INT': 'error',
-            'FLT': 'error',
-            'CHAR': 'error',
-            'STR': 'error',
-            'BOOL': 'BOOL',
-            'NULL': 'BOOL'
+        "BOOL": {
+            "INT": "error",
+            "FLT": "error",
+            "CHAR": "error",
+            "STR": "error",
+            "BOOL": "BOOL",
+            "NULL": "BOOL",
         },
-
-        'NULL': {
-            'INT': 'error',
-            'FLT': 'error',
-            'CHAR': 'error',
-            'STR': 'error',
-            'BOOL': 'error',
-            'NULL': 'error'
-        }
+        "NULL": {
+            "INT": "error",
+            "FLT": "error",
+            "CHAR": "error",
+            "STR": "error",
+            "BOOL": "error",
+            "NULL": "error",
+        },
     }
 
     def considerate(symbol_1, symbol_op, symbol_2):
         # When input is in string
         if [type(symbol_1), type(symbol_op), type(symbol_2)] == [str, str, str]:
-            
+
             # Convert symbol into correct type
-            if symbol_op in ["ADD", "SUB", "DIV", "MUL", "MOD", "ADDEQ", "SUBEQ", "DIVEQ", "MODEQ", "MODEQ"]:
+            if symbol_op in [
+                "ADD",
+                "SUB",
+                "DIV",
+                "MUL",
+                "MOD",
+                "ADDEQ",
+                "SUBEQ",
+                "DIVEQ",
+                "MODEQ",
+                "MODEQ",
+            ]:
                 symbol_op = "operation"
             elif symbol_op in ["LT", "GT", "LTE", "GTE"]:
                 symbol_op = "comparison"
@@ -142,17 +155,19 @@ class SemanticTable:
             symbol_2 = symbol_2.type
             symbol_op = symbol_op.type
 
-        if not(symbol_1 in SemanticTable.types) or not(symbol_2 in SemanticTable.types):
-            return 'error'
+        if not (symbol_1 in SemanticTable.types) or not (
+            symbol_2 in SemanticTable.types
+        ):
+            return "error"
 
-        elif symbol_op == 'operation':
+        elif symbol_op == "operation":
             return SemanticTable.__operations[symbol_1][symbol_2]
 
-        elif symbol_op == 'comparison':
+        elif symbol_op == "comparison":
             return SemanticTable.__comparison[symbol_1][symbol_2]
 
-        elif symbol_op == 'matching': 
-            return 'BOOL'
+        elif symbol_op == "matching":
+            return "BOOL"
 
         else:
-            return 'error'
+            return "error"
--- symbol_tables.py
+++ symbol_tables.py
@@ -1,57 +1,60 @@
 import collections
 from symbol import *
 
+
 class VariableTable(object):
     def __init__(self):
         self.variables = {}
 
     def set_variable(self, symbol, value):
         self.variables[symbol.name] = [symbol.type, value]
-    
+
     def get_variable(self, name):
         return self.variables[name]
-    
+
     def lookup_variable(self, name):
         if name in self.variables:
             return True
         else:
             return False
-    
+
     def print_VariableTable(self):
         print("NAME TYPE VALUE ")
         for s in self.variables:
-            print(s + "   " + self.variables[s][0] + "     " +  str(self.variables[s][1]))
+            print(
+                s + "   " + self.variables[s][0] + "     " + str(self.variables[s][1])
+            )
             print()
-        
+
 
 class FunctionTable(object):
     def __init__(self):
         self.functions = {}
 
     def set_function(self, name, type, parameters, variable_table):
-        self.functions[name] = {'t': type, 'p' : parameters, 'vt' : variable_table}
-    
+        self.functions[name] = {"t": type, "p": parameters, "vt": variable_table}
+
     def set_function_variable_table_at(self, name):
-        self.functions[name]['vt'] = VariableTable()
-        for symbol in self.functions[name]['p']:
-            self.functions[name]['vt'].set_variable(symbol, None)
+        self.functions[name]["vt"] = VariableTable()
+        for symbol in self.functions[name]["p"]:
+            self.functions[name]["vt"].set_variable(symbol, None)
 
     def get_function(self, name):
         return self.functions[name]
 
     def get_function_type(self, name):
-         return self.functions[name]['t']
+        return self.functions[name]["t"]
 
     def get_function_parameters(self, name):
-         return self.functions[name]['p']
-        
+        return self.functions[name]["p"]
+
     def get_function_variable_table(self, name):
-         return self.functions[name]['vt']
-    
+        return self.functions[name]["vt"]
+
     def erase_function_variable_table(self, name):
-        del self.functions[name]['vt']
-        self.functions[name]['vt'] = None
-        
+        del self.functions[name]["vt"]
+        self.functions[name]["vt"] = None
+
     def lookup_function(self, name):
         if name in self.functions:
             return True
@@ -61,12 +64,18 @@ class FunctionTable(object):
     def print_FuncTable(self):
         print("name, type, parameters, variable_table")
         for name in self.functions:
-            print(name + "   " + str(self.functions[name]['t']) + " " + str(self.functions[name]['p']))
+            print(
+                name
+                + "   "
+                + str(self.functions[name]["t"])
+                + " "
+                + str(self.functions[name]["p"])
+            )
             print("VARTABLE")
-            if self.functions[name]['vt'] != None:
-                self.functions[name]['vt'].print_VariableTable()
+            if self.functions[name]["vt"] != None:
+                self.functions[name]["vt"].print_VariableTable()
             else:
-                print(self.functions[name]['vt'])
+                print(self.functions[name]["vt"])
             print()
 
 
@@ -78,63 +87,60 @@ class State(object):
     def set_state(self, table, opt=None):
         self.table = table
         self.opt = opt
-    
+
 
 class StateTable(object):
-        def __init__(self):
-            self.states = []
+    def __init__(self):
+        self.states = []
 
-        def push_state(self, state):
-            self.states.append(state)
+    def push_state(self, state):
+        self.states.append(state)
 
-        def get_curr_state(self):
-            return self.states[-1]
+    def get_curr_state(self):
+        return self.states[-1]
 
-        def get_curr_state_table(self):
-            curr = State()
-            return self.states[-1].table
-        
-        def get_curr_state_opt(self):
-            return self.states[-1].opt
-        
-        def set_curr_state_opt(self, opt):
-            self.states[-1].opt = opt
+    def get_curr_state_table(self):
+        curr = State()
+        return self.states[-1].table
 
-        def remove_curr_state_opt(self):
-            self.states[-1].opt = None
-        
-        def pop_curr_state(self):
-            self.states.pop()
-        
-        def get_and_pop(self):
-            s = self.get_curr_state()
-            self.pop_curr_state()
-            return s
+    def get_curr_state_opt(self):
+        return self.states[-1].opt
 
-        def get_global_table(self):
-            return self.states[0].table
+    def set_curr_state_opt(self, opt):
+        self.states[-1].opt = opt
 
-        def isEmpty(self):
-            return (self.states.count() == 0)
+    def remove_curr_state_opt(self):
+        self.states[-1].opt = None
 
-        def isValidState(self, state, functiontable):
-            otherValidStates = ["funcD", "funcC", "noVar"]
-            if not state.isEmpty():
-                if state.table not in functiontable.keys():
-                    if state.table not in otherValidStates:
-                        return False
-                    else: 
-                        return True
-                else: 
+    def pop_curr_state(self):
+        self.states.pop()
+
+    def get_and_pop(self):
+        s = self.get_curr_state()
+        self.pop_curr_state()
+        return s
+
+    def get_global_table(self):
+        return self.states[0].table
+
+    def isEmpty(self):
+        return self.states.count() == 0
+
+    def isValidState(self, state, functiontable):
+        otherValidStates = ["funcD", "funcC", "noVar"]
+        if not state.isEmpty():
+            if state.table not in functiontable.keys():
+                if state.table not in otherValidStates:
+                    return False
+                else:
                     return True
             else:
-                return False
-        
-        def print_StateTable(self):
-            print("name, opt")
-            for name in self.states:
-                print(str(name.table) + "   " + str(name.opt))
-                print()
+                return True
+        else:
+            return False
 
-             
-        
+    def print_StateTable(self):
+        print("name, opt")
+        for name in self.states:
+            print(str(name.table) + "   " + str(name.opt))
+            print()
