a42a1b1dc1e95c606740a4de2aa2adbd585c6481
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-15 22:04:50 -0500

    Revert "Added Unnit tests. They crash"
    This reverts commit 4a593dba048c76d94141af133552ab67a511abda.

--- symbol.py
+++ symbol.py
@@ -1,22 +1,7 @@
-import numpy as np
-
 class Symbol(object):
-    # All memory sizes are expressed in bytes.
-    __memory_sizes = { 
-        "INT": 4,
-        "FLT": 4,
-        "CHAR": 1,
-        "STR": 1,
-        "BOOL": 1,
-        "NULL": 1,
-    }
-
-    def __init__(self, name=None, type=None, dimension_sizes=[], direction=None):
+    def __init__(self, name=None, type=None):
         self.name = name
         self.type = type
-        self.dimension_sizes = dimension_sizes
-        self.dimensions = len(dimension_sizes)
-        self.direction = direction
 
     def __eq__(self, quad):
         return self.name == quad.name and self.type == quad.type
@@ -34,19 +19,5 @@ class Symbol(object):
         return self.name
 
     def print_symbol(self):
-        if self.name:
-            print("VAR: ", self.name)
-
-        if self.type:
-            print("TYPE: ", self.type)
-
-        if len(self.dimension_sizes):
-            print("DIMENSIONS: ", self.dimensions)
-            print("DIMENSION_SIZES: ", self.dimension_sizes)
-
-        if self.direction:
-            print("DIRECTION: ", self.direction)
-
-    # TODO: No calcula bien las dimensiones
-    def memory_size(self):
-        return Symbol.__memory_sizes[self.type] * (np.prod(self.dimension_sizes) + 1)
+        print("VAR: " + self.name)
+        print("TYPE: " + self.type)
