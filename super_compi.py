78b4080cda33784285b3bf310d31101f0f11f0e6
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-29 18:41:29 -0500

    Added small cases

--- super_compi.py
+++ super_compi.py
@@ -1,5 +1,5 @@
 from execute import *
 
-Executer("compilador/tests/test_14.milf").run(
+Executer("compilador/tests/test_25.milf").run(
     print_running=False, print_quads=True, print_instructions=False, run_game=False
 )
