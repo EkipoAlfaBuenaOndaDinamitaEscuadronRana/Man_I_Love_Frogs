e0319f342b7e7c28d7b3179d4d5ab7c4b291418e
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-29 18:43:19 -0500

    Solved real merge conflicts

--- super_compi.py
+++ super_compi.py
@@ -1,14 +1,9 @@
 from execute import *
 
-<<<<<<< HEAD
-Executer("compilador/tests/test_25.milf").run(
-    print_running=False, print_quads=True, print_instructions=False, run_game=False
-=======
 Executer("compilador/tests/test_23.milf").run(
     print_running=False,
     print_pre_quads=True,
     print_post_quads=False,
     print_instructions=False,
     run_game=False,
->>>>>>> vm/functions
 )
