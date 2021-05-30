0bc63a251ebbc69d5674d63849d605b13f0d6aa6
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-29 21:13:59 -0500

    Small changes for rosita

--- super_compi.py
+++ super_compi.py
@@ -1,9 +1,9 @@
 from execute import *
 
-Executer("compilador/tests/test_25.milf").run(
+Executer("compilador/tests/test_12.milf").run(
     print_running=False,
     print_pre_quads=True,
     print_post_quads=False,
     print_instructions=False,
-    run_game=False,
+    run_game=True,
 )
