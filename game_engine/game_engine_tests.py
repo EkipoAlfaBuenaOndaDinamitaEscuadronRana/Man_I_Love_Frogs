20a7d795073e807c0c5383176c34e0bff078f5d7
Adrian-Garcia <adriangarlop@hotmail.com>
2021-06-01 23:30:20 -0500

    Tried to solved game engine tests

--- game_engine/game_engine_tests.py
+++ game_engine/game_engine_tests.py
@@ -4,6 +4,7 @@ from game_engine.engine import *
 from execute import *
 import execute
 import unittest
+from unittest.mock import MagicMock
 
 
 class TestEngine(unittest.TestCase):
@@ -16,34 +17,45 @@ class TestEngine(unittest.TestCase):
             Instruction("pikachu", "JL", 3),
         ]
 
+        pikachu = characters["pikachu"]
+        cols = Constants.DISPLAY_WIDTH // Constants.FROG_WIDTH
+        rows = Constants.DISPLAY_HEIGHT // Constants.FROG_HEIGHT
+        board = [[None for x in range(cols)] for i in range(rows)]
+        board[2][2] = pikachu
+
         # Starting on point x = 100 and y = 100
         self.assertEqual(characters["pikachu"].x, 100)
         self.assertEqual(characters["pikachu"].y, 100)
 
         # Moving down
-        Engine.instruction_movement(instructions.pop(0), characters)
+        Engine.instruction_movement(instructions.pop(0), characters, board)
         self.assertEqual(characters["pikachu"].y, 250)
 
         # Moving up
-        Engine.instruction_movement(instructions.pop(0), characters)
+        Engine.instruction_movement(instructions.pop(0), characters, board)
         self.assertEqual(characters["pikachu"].y, 0)
 
         # Moving right
-        Engine.instruction_movement(instructions.pop(0), characters)
+        Engine.instruction_movement(instructions.pop(0), characters, board)
         self.assertEqual(characters["pikachu"].x, 150)
 
         # Moving left
-        Engine.instruction_movement(instructions.pop(0), characters)
+        Engine.instruction_movement(instructions.pop(0), characters, board)
         self.assertEqual(characters["pikachu"].x, 0)
 
 
 class TestCharacter(unittest.TestCase):
     def _test_move_down(self):
+
         character = Character(0, 50, 30, 30, 50)
+        cols = Constants.DISPLAY_WIDTH // Constants.FROG_WIDTH
+        rows = Constants.DISPLAY_HEIGHT // Constants.FROG_HEIGHT
+        board = [[None for x in range(cols)] for i in range(rows)]
+        board[0][1] = character
         display_height = 500
 
         self.assertEqual(character.y, 50)
-        character.move_down(display_height, 1)
+        character.move_down(1, board)
         self.assertEqual(character.y, 100)
 
     def _test_move_up(self):
